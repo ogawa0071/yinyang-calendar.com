9e7355b5fdebce99254a9c1247606e8feef69956
Futa Ogawa <ogawa0071@gmail.com>
2019-01-16 00:14:45 +0900

    `npx create-nuxt-app`

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+/* eslint-env browser */
+;(() => {
+  const fixHeight = () => {
+    const html = document.documentElement
+    const documentHight = document.body.offsetHeight
+    const windowHight = window.innerHeight
+    if (documentHight < windowHight) {
+      html.style.height = `${window.innerHeight}px`
+    } else {
+      html.style.height = ''
+    }
+  }
+
+  window.onload = fixHeight
+  window.addEventListener('resize', fixHeight)
+})()
