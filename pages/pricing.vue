462890c5d92f790b6c7cf52db4f3c3e55b4d7a94
Futa Ogawa <futa@Futa-Ogawas-MacBook-Pro.local>
2019-11-09 21:57:21 +0900

    消費税の変更を反映

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -18,51 +18,60 @@
               <tbody>
                 <tr>
                   <td>1</td>
-                  <td>¥600</td>
-                  <td>¥48</td>
-                  <td>¥648</td>
+                  <td>¥700</td>
+                  <td>¥70</td>
+                  <td>¥770</td>
                   <td>¥120</td>
                 </tr>
                 <tr>
                   <td>10</td>
-                  <td>¥5,400</td>
-                  <td>¥432</td>
-                  <td>¥5,832</td>
-                  <td>¥185</td>
+                  <td>¥6,300</td>
+                  <td>¥630</td>
+                  <td>¥6,930</td>
+                  <td>¥188</td>
                 </tr>
                 <tr>
                   <td>20</td>
-                  <td>¥10,200</td>
-                  <td>¥816</td>
-                  <td>¥11,016</td>
-                  <td>¥185</td>
+                  <td>¥11,200</td>
+                  <td>¥1,120</td>
+                  <td>¥12,320</td>
+                  <td>¥188</td>
                 </tr>
                 <tr>
                   <td>30</td>
-                  <td>¥14,400</td>
-                  <td>¥1,152</td>
-                  <td>¥15,552</td>
-                  <td><a 
-                    href="http://www.kuronekoyamato.co.jp/estimate/kanto.html" 
-                    target="_blank">こちら</a></td>
+                  <td>¥15,750</td>
+                  <td>¥1,570</td>
+                  <td>¥15,750</td>
+                  <td>
+                    <a
+                      href="http://www.kuronekoyamato.co.jp/estimate/kanto.html"
+                      target="_blank"
+                    >こちら</a>
+                  </td>
                 </tr>
                 <tr>
                   <td>50</td>
-                  <td>¥21,000</td>
-                  <td>¥1,680</td>
-                  <td>¥22,680</td>
-                  <td><a 
-                    href="http://www.kuronekoyamato.co.jp/estimate/kanto.html" 
-                    target="_blank">こちら</a></td>
+                  <td>¥24,500</td>
+                  <td>¥2,450</td>
+                  <td>¥26,950</td>
+                  <td>
+                    <a
+                      href="http://www.kuronekoyamato.co.jp/estimate/kanto.html"
+                      target="_blank"
+                    >こちら</a>
+                  </td>
                 </tr>
                 <tr>
                   <td>100</td>
-                  <td>¥36,000</td>
-                  <td>¥2,880</td>
-                  <td>¥38,880</td>
-                  <td><a 
-                    href="http://www.kuronekoyamato.co.jp/estimate/kanto.html" 
-                    target="_blank">こちら</a></td>
+                  <td>¥42,000</td>
+                  <td>¥4,200</td>
+                  <td>¥46,200</td>
+                  <td>
+                    <a
+                      href="http://www.kuronekoyamato.co.jp/estimate/kanto.html"
+                      target="_blank"
+                    >こちら</a>
+                  </td>
                 </tr>
               </tbody>
             </table>
@@ -70,10 +79,8 @@
           <div class="col-md-8 col-md-offset-2 banner">
             <ul class="list-inline">
               <li class="center-block">
-                <a 
-                  href="mailto:info@yinyang-calendar.com" 
-                  class="btn btn-default btn-lg">
-                  <i class="fa fa-envelope fa-fw"/>
+                <a href="mailto:info@yinyang-calendar.com" class="btn btn-default btn-lg">
+                  <i class="fa fa-envelope fa-fw" />
                   <span class="network-name">info@yinyang-calendar.com</span>
                 </a>
               </li>
